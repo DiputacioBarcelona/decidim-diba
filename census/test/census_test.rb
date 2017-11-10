3cccbe3cdabc826a283e542596b247f68f19a1b6
danigb <danigb@gmail.com>
2017-11-10 10:25:53 +0100

    Rubocop fixings

--- census/test/census_test.rb
+++ census/test/census_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class Census::Test < ActiveSupport::TestCase
-  test "truth" do
+
+  test 'truth' do
     assert_kind_of Module, Census
   end
+
 end
--- census/test/dummy/config/routes.rb
+++ census/test/dummy/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  mount Census::Engine => "/census"
+  mount Census::Engine => '/census'
 end
