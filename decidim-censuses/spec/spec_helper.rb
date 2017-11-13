c3fabaa7e25d2581ab302866d990a1bb486848c7
danigb <danigb@gmail.com>
2017-11-13 13:28:03 +0100

    Test AuthorizeWithAge

--- decidim-censuses/spec/spec_helper.rb
+++ decidim-censuses/spec/spec_helper.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require "decidim/dev"
+require 'decidim/dev'
 
-ENV["ENGINE_NAME"] = File.dirname(File.dirname(__FILE__)).split("/").last
+ENV['ENGINE_NAME'] = File.dirname(File.dirname(__FILE__)).split('/').last
 
-Decidim::Dev.dummy_app_path = File.expand_path(File.join("..", "spec", "decidim_dummy_app"))
+Decidim::Dev.dummy_app_path = File.expand_path(File.join('..', 'spec', 'decidim_dummy_app'))
 
-require "decidim/dev/test/base_spec_helper"
+require 'decidim/dev/test/base_spec_helper'
 
 RSpec.configure do |config|
   config.before(:each) do
