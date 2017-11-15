35b4659a826fc9cf3caea216929236d42a6a732d
danigb <danigb@gmail.com>
2017-11-15 13:17:46 +0100

    Add custom authorization form

--- decidim-diba_census_api/app/services/diba_census_api_service.rb
+++ decidim-diba_census_api/app/services/diba_census_api_service.rb
@@ -9,24 +9,26 @@ class DibaCensusApiService
 
   def self.build_message(_params)
     {
-      ope: {
-        apl: 'PAD',
-        tobj: 'HAB',
-        cmd: 'DATOSHABITANTES',
-        ver: '2.0'
-      },
-      sec: {
-        cli: 'decidim',
-        org: 0,
-        ent: 998,
-        usu: 'Decidim',
-        pwd: 'Decidim2017'
-      },
-      par: {
-        codigo_tipo_documento: 1,
-        documento: '58958982T',
-        fecha_nacimiento: '05/05/1991',
-        busqueda_exacta: 0
+      e: {
+        ope: {
+          apl: 'PAD',
+          tobj: 'HAB',
+          cmd: 'DATOSHABITANTES',
+          ver: '2.0'
+        },
+        sec: {
+          cli: 'ACCEDE',
+          org: 0,
+          ent: 998,
+          usu: 'Decidim',
+          pwd: 'Decidim2017'
+        },
+        par: {
+          codigo_tipo_documento: 1,
+          documento: '58958982T',
+          fecha_nacimiento: '05/05/1991',
+          busqueda_exacta: 0
+        }
       }
     }
   end
