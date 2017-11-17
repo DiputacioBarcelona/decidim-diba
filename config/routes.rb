66655ddb6bd3ab6803edf2a5b7b0ed73e1af04c5
danigb <danigb@gmail.com>
2017-11-17 14:46:48 +0100

    Add organization census api configuration - Add table columns via migration - Add admin interface to show and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount Decidim::Core::Engine => '/'
+  mount Decidim::DibaCensusApi::AdminEngine => ''
 
   resource :system_status, only: :show
 end
--- decidim-diba_census_api/lib/decidim/diba_census_api.rb
+++ decidim-diba_census_api/lib/decidim/diba_census_api.rb
@@ -1,4 +1,6 @@
 require 'decidim/diba_census_api/engine'
+require 'decidim/diba_census_api/admin'
+require 'decidim/diba_census_api/admin_engine'
 
 module Decidim
   # Base module for this Engine
