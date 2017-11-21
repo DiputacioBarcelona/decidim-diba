1f14c3971d547aed0695d5b5b6d90ef43ff123d6
danigb <danigb@gmail.com>
2017-11-21 09:41:34 +0100

    Remove unused code

--- decidim-censuses/app/assets/config/censuses_manifest.js
+++
@@ -1,2 +0,0 @@
-//= link_directory ../javascripts/census .js
-//= link_directory ../stylesheets/census .css
--- decidim-censuses/app/helpers/decidim/censuses/application_helper.rb
+++
@@ -1,6 +0,0 @@
-module Decidim
-  module Censuses
-    module ApplicationHelper
-    end
-  end
-end
