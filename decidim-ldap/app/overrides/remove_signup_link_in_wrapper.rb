6341aa2ff312b225d36076a8edb2af93255c5452
Laura Jaime <laura.jv@coditramuntana.com>
2021-02-11 11:36:27 +0100

    Change some overrides with Deface

--- decidim-ldap/app/overrides/remove_signup_link_in_wrapper.rb
+++ decidim-ldap/app/overrides/remove_signup_link_in_wrapper.rb
@@ -1,6 +1,4 @@
-unless ActiveModel::Type::Boolean.new.cast(ENV['DOCKER'])
-  Deface::Override.new(virtual_path: 'layouts/decidim/_wrapper',
+Deface::Override.new(virtual_path: 'layouts/decidim/_wrapper',
                      name: 'remove_signup_link_in_wrapper',
                      replace: "erb[loud]:contains('if current_organization.sign_up_enabled?')",
                      text: '<% if current_organization.sign_up_enabled? || current_organization.ldap? %>')
-end
