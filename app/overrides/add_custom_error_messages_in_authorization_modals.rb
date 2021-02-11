61e1380ccd82e113376f59a6c0c68316ac3b6478
Laura Jaime <laura.jv@coditramuntana.com>
2021-02-11 11:36:27 +0100

    Change some overrides with Deface

--- app/overrides/add_custom_error_messages_in_authorization_modals.rb
+++ app/overrides/add_custom_error_messages_in_authorization_modals.rb
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 Deface::Override.new(virtual_path: 'decidim/authorization_modals/_content',
                      name: 'add_custom_error_messages_in_authorization_modals',
                      replace: '<li>',
+=======
+Deface::Override.new(virtual_path: 'layouts/decidim/_wrapper',
+                     name: 'radd_custom_error_messages_in_authorization_modals',
+                     replace: '<li><strong><%= t ".#{status.code}.invalid_field", field: t("#{status.handler_name}.fields.#{field}", scope: "decidim.authorization_handlers"), value: value ? "(#{value})" : "" %></strong></li>',
+>>>>>>> Change some overrides with Deface
                      text: '
                       <% case field %>
                         <% when :birthdate %>
