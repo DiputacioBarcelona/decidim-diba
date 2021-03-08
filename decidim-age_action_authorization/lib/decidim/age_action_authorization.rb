45d51039e765b5cf494c0715b69d2e046fe0d9a5
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-03-08 17:35:45 +0100

    Rubocopify

--- decidim-age_action_authorization/lib/decidim/age_action_authorization.rb
+++ decidim-age_action_authorization/lib/decidim/age_action_authorization.rb
@@ -1,5 +1,7 @@
-require 'decidim/age_action_authorization/version'
-require 'decidim/age_action_authorization/authorizer'
+# frozen_string_literal: true
+
+require "decidim/age_action_authorization/version"
+require "decidim/age_action_authorization/authorizer"
 
 module Decidim
   module AgeActionAuthorization
--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
-RSpec.describe "Overrides" do
-  # scenario "remove override to allow non signed in users to be invited to the promoter committee" do
-  #   # remove app/permissions/decidim/initiatives/permissions.rb, see docs/overrides.md for details
-  #   expect(Decidim.version).to be < "0.22"
-  # end
-end
+# RSpec.describe "Overrides" do
+# scenario "remove override to allow non signed in users to be invited to the promoter committee" do
+#   # remove app/permissions/decidim/initiatives/permissions.rb, see docs/overrides.md for details
+#   expect(Decidim.version).to be < "0.22"
+# end
+# end
