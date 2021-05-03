22af2b62ede99c61a87bcba1e52d7cb7d37e12f8
Laura Jaime <laura.jv@coditramuntana.com>
2021-05-03 16:13:57 +0200

    Fix diba-census-api workflow

--- decidim-diba_census_api/spec/spec_helper.rb
+++ decidim-diba_census_api/spec/spec_helper.rb
@@ -12,6 +12,6 @@ require "social-share-button"
 ENV["ENGINE_NAME"] = File.dirname(__dir__).split("/").last
 
 Decidim::Dev.dummy_app_path =
-  File.expand_path(File.join("..", "spec", "decidim_dummy_app"))
+  File.expand_path(File.join("spec", "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
