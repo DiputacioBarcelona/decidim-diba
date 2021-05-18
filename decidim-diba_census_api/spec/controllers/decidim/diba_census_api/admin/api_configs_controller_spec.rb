5a1bf6429d2723e9307d8db1f0d66b9a4da5adf3
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-05-18 17:15:22 +0200

    Make decidim-diba_census_api tests succeed

--- decidim-diba_census_api/spec/controllers/decidim/diba_census_api/admin/api_configs_controller_spec.rb
+++ decidim-diba_census_api/spec/controllers/decidim/diba_census_api/admin/api_configs_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Decidim::DibaCensusApi::Admin::ApiConfigsController,
   end
 
   let(:user) do
-    FactoryBot.create :user, :confirmed, organization: organization, admin: true
+    FactoryBot.create :user, :confirmed, :admin_terms_accepted, organization: organization, admin: true, nickname: "nickname"
   end
 
   before do
