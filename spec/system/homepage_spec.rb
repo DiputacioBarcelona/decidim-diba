33eb9bd22fba9ea0500719d2d6cef811b4e25649
Laura Jaime <laura.jv@coditramuntana.com>
2022-02-04 12:00:33 +0100

    Fix app tests

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -2,9 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe "Homepage", type: :system do
-  include Decidim::SanitizeHelper
-
+describe "Homepage", type: :system do
   let!(:organization) do
     create(
       :organization,
@@ -12,26 +10,29 @@ RSpec.describe "Homepage", type: :system do
       default_locale: :en,
       available_locales: [:ca, :en, :es]
     )
-  end
-  let!(:hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :hero, settings: { "welcome_text_en"=>"Welcome to Decidim DiBa" } }
-  let!(:sub_hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :sub_hero }
 
-  before do
-    I18n.locale= :en
-    switch_to_host(organization.host)
-    visit decidim.root_path
-  end
+    before do
+      I18n.locale= :en
+      create :content_block, organization: organization, scope_name: :homepage, manifest_name: :hero
+      create :content_block, organization: organization, scope_name: :homepage, manifest_name: :sub_hero
+      create :content_block, organization: organization, scope_name: :homepage, manifest_name: :highlighted_content_banner
+      create :content_block, organization: organization, scope_name: :homepage, manifest_name: :how_to_participate
+      create :content_block, organization: organization, scope_name: :homepage, manifest_name: :footer_sub_hero
 
-  it "loads and shows organization name and main blocks" do
-    visit decidim.root_path
-
-    expect(page).to have_content("Decidim DiBa")
-    within "section.hero .hero__container" do
-      expect(page).to have_content("Welcome to Decidim DiBa")
+      switch_to_host(organization.host)
     end
-    within "section.subhero" do
-      subhero_msg= translated(organization.description).gsub(%r{</p>\s+<p>}, "<br><br>").gsub(%r{<p>(((?!</p>).)*)</p>}mi, "\\1")
-      expect(page).to have_content(subhero_msg)
+
+    it "loads and shows organization name and main blocks" do
+      visit decidim.root_path
+
+      expect(page).to have_content("Decidim DiBa")
+      within "section.hero .hero__container" do
+        expect(page).to have_content("Welcome to Decidim DiBa")
+      end
+      within "section.subhero" do
+        subhero_msg= translated(organization.description).gsub(%r{</p>\s+<p>}, "<br><br>").gsub(%r{<p>(((?!</p>).)*)</p>}mi, "\\1")
+        expect(page).to have_content(subhero_msg)
+      end
     end
   end
 end
