b7a311d12913af648d35a860b539eb4dc6863ab9
Laura Jaime <laura.jv@coditramuntana.com>
2022-03-01 16:58:17 +0100

    Force decidim-awesome version

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -4,6 +4,9 @@ require "rails_helper"
 
 RSpec.describe "Overrides" do
   it "remove override export proposals to csv in all locales" do
+    # ACTION ->
+    # run decidim_decidim_awesome:webpacker:install when upgrade Decidim to v0.25
+
     expect(Decidim.version).to be < "0.25"
   end
 end
