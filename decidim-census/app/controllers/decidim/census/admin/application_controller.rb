2825d5b81f588af5c34f945fdb068cfd48095b71
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-11-23 07:36:54 +0100

    Remove wrong comment

--- decidim-census/app/controllers/decidim/census/admin/application_controller.rb
+++ decidim-census/app/controllers/decidim/census/admin/application_controller.rb
@@ -5,11 +5,6 @@ module Decidim
     module Admin
       # This controller is the abstract class from which all other controllers of
       # this engine inherit.
-      #
-      # Note that it inherits from `Decidim::Components::BaseController`, which
-      # override its layout and provide all kinds of useful methods.
-      # class ApplicationController < Decidim::Admin::Components::BaseController
-
       class ApplicationController < Decidim::Admin::ApplicationController
         private
 
