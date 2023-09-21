b3b96abb6122e47a0267c2a3175e0abb90ab917e
Laura Jaime <laura.jv@coditramuntana.com>
2023-09-21 09:44:51 +0200

    Remove overrides locales

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -3,17 +3,6 @@
 require "rails_helper"
 
 RSpec.describe "Overrides" do
-  it "remove overrides after upgrading to v0.25" do
-    # - Remove override export proposals to csv in all locales
-    # - Remove :ca and :es transations.
-    #   After upgrading to v0.25 the following keys should already be translated in Decidim
-    #   and can be removed from diba:
-    #     - decidim.events.users.user_officialized.*
-    #     - decidim.events.verifications.verify_with_managed_user.*
-    # - Run decidim_decidim_awesome:webpacker:install when upgrade Decidim to v0.25
-    expect(Decidim.version).to be < "0.25"
-  end
-
   it "remove config/initializers/doorkeeper.rb after Decidim v0.28" do
     # After Decidim v0.28 as it is already initialized in that version:
     # - remove config/initializers/doorkeeper.rb
