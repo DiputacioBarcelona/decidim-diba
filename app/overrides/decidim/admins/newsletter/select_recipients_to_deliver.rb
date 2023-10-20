adbe2f87be066032ff9b25596a640084e4012d7c
Laura Jaime <laura.jv@coditramuntana.com>
2023-10-20 10:20:48 +0200

    Rubocopify

--- app/overrides/decidim/admins/newsletter/select_recipients_to_deliver.rb
+++ app/overrides/decidim/admins/newsletter/select_recipients_to_deliver.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-
 unless ActiveModel::Type::Boolean.new.cast(ENV["DOCKER"])
-   Deface::Override.new(virtual_path: "decidim/admin/newsletters/select_recipients_to_deliver",
-                     name: "add_new_user_segmentation",
-                     insert_after: ".grid-padding-x:last-child",
-                     original: "27433f49f16b5a28709d507f1fe5c781022b15e8",
-                     text: '
+  Deface::Override.new(virtual_path: "decidim/admin/newsletters/select_recipients_to_deliver",
+                       name: "add_new_user_segmentation",
+                       insert_after: ".grid-padding-x:last-child",
+                       original: "27433f49f16b5a28709d507f1fe5c781022b15e8",
+                       text: '
                         <%= render partial: "select_single_users", locals: { f: f } %>
                       ')
 end
