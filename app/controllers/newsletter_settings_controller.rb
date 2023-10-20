fe145cf221b773f15ed16eb8b0440b88370b4a7e
Laura Jaime <laura.jv@coditramuntana.com>
2023-10-20 21:02:25 +0200

    Fix some errors

--- app/controllers/newsletter_settings_controller.rb
+++ app/controllers/newsletter_settings_controller.rb
@@ -2,12 +2,18 @@
 
 class NewsletterSettingsController < ApplicationController
   def show
-    user = Decidim::User.where(id: params[:user_id], organization: current_organization)
+    user = Decidim::User.find_by(id: params[:user_id], organization: current_organization)
 
     if user.newsletter_notifications_at.present?
       render json: { message: "ok", status: 200 }
     else
       render json: { message: t(".user_without_newsletter_settings"), status: 403 }
     end
+  end
+
+  private
+
+  def current_organization
+    current_user.organization
   end
 end
