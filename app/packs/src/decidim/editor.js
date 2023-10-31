cadf646eafa647ed3b8d6379a36f716dcf401bb4
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-10-31 13:02:00 +0100

    Make editor override take into account decidim_awesome
    As this file is executed before

--- app/packs/src/decidim/editor.js
+++ app/packs/src/decidim/editor.js
@@ -1,4 +1,7 @@
 /**
 * Override Decidim's editor so it is not executed.does not modify our config.
 */
-// const noop = () => {};
+import {destroyQuillEditor, createQuillEditor, createMarkdownEditor} from "./decidim_awesome/editors/editor.js";
+
+export {destroyQuillEditor, createQuillEditor, createMarkdownEditor};
+export default createQuillEditor;
