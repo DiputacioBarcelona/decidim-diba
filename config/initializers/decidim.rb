954cb096d0e4dc92ed9f3ae57d60ff54c9342a95
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-07-22 15:12:17 +0200

    Add content_security_policies extra

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -398,6 +398,19 @@ Decidim.configure do |config|
   #   }
   # ]
 
+  # Defines additional content security policies following the structure
+  # Read more: https://docs.decidim.org/en/develop/configure/initializer#_content_security_policy
+  config.content_security_policies_extra = {
+    "default-src" => %w('self' 'unsafe-inline'),
+    "script-src" => %w('self' 'unsafe-inline' 'unsafe-eval' ajax.cloudflare.com),
+    "style-src" => %w('self' 'unsafe-inline' fonts.googleapis.com maxcdn.bootstrapcdn.com),
+    "img-src" => %w('self' *.hereapi.com data: *.amazonaws.com),
+    "font-src" => %w('self' fonts.gstatic.com maxcdn.bootstrapcdn.com),
+    "connect-src" => %w('self' *.hereapi.com *.jsdelivr.net *.amazonaws.com fonts.googleapis.com maxcdn.bootstrapcdn.com),
+    "frame-src" => %w('self' *.youtube.com www.youtube-nocookie.com),
+    "media-src" => %w('self')
+  }
+
   # Admin admin password configurations
   Rails.application.secrets.dig(:decidim, :admin_password, :strong).tap do |strong_pw|
     # When the strong password is not configured, default to true
