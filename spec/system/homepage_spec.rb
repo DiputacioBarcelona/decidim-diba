e3819652473f534e3112c66bb77fec20b0b928fe
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-06-05 17:19:32 +0200

    Fix homepage test

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -6,33 +6,35 @@ describe "Homepage" do
   let!(:organization) do
     create(
       :organization,
-      name: "Decidim DiBa",
+      name: { ca: "Decidim DiBa", es: "Decidim DiBa", en: "Decidim DiBa" },
+      description: { ca: "Descripció ca", es: "Descripción es", en: "Description en" },
       default_locale: :en,
       available_locales: [:ca, :en, :es]
     )
+  end
 
-    before do
-      I18n.locale = :en
+  before do
+    I18n.with_locale(:en) do
       create :content_block, organization:, scope_name: :homepage, manifest_name: :hero
       create :content_block, organization:, scope_name: :homepage, manifest_name: :sub_hero
       create :content_block, organization:, scope_name: :homepage, manifest_name: :highlighted_content_banner
       create :content_block, organization:, scope_name: :homepage, manifest_name: :how_to_participate
       create :content_block, organization:, scope_name: :homepage, manifest_name: :footer_sub_hero
-
-      switch_to_host(organization.host)
     end
 
-    it "loads and shows organization name and main blocks" do
-      visit decidim.root_path
+    switch_to_host(organization.host)
+  end
 
-      expect(page).to have_content("Decidim DiBa")
-      within "section.hero .hero__container" do
-        expect(page).to have_content("Welcome to Decidim DiBa")
-      end
-      within "section.subhero" do
-        subhero_msg = translated(organization.description).gsub(%r{</p>\s+<p>}, "<br><br>").gsub(%r{<p>(((?!</p>).)*)</p>}mi, "\\1")
-        expect(page).to have_content(subhero_msg)
-      end
+  it "loads and shows organization name and main blocks" do
+    visit decidim.root_path
+
+    expect(page).to have_content("Decidim DiBa")
+    within "section.hero__container .hero" do
+      expect(page).to have_content("Welcome to Decidim DiBa")
+    end
+
+    within "section#sub_hero" do
+      expect(page).to have_content(translated(organization.description))
     end
   end
 end
