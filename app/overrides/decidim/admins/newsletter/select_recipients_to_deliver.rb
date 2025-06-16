867d0c44eab507c78b1174f3583515ba86518b9a
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-06-16 16:53:24 +0200

    Fix newsletter settings override

--- app/overrides/decidim/admins/newsletter/select_recipients_to_deliver.rb
+++ app/overrides/decidim/admins/newsletter/select_recipients_to_deliver.rb
@@ -3,8 +3,8 @@
 unless ActiveModel::Type::Boolean.new.cast(ENV.fetch("DOCKER", nil))
   Deface::Override.new(virtual_path: "decidim/admin/newsletters/select_recipients_to_deliver",
                        name: "add_new_user_segmentation",
-                       insert_after: ".grid-padding-x:last-child",
-                       original: "27433f49f16b5a28709d507f1fe5c781022b15e8",
+                       insert_after: "div#send_newsletter_to_participants",
+                       original: "e5a692c0a681a7bd3160ee02d18e000963d40ddd",
                        text: '
                         <%= render partial: "select_single_users", locals: { f: f } %>
                       ')
